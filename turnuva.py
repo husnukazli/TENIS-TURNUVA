6c64bb6d84bb5ba507ef3f36f490fc1e513cbe46
husnukazli <husnukazli@gmail.com>
2026-06-21 18:18:00 +0300

    Update turnuva.py

--- turnuva.py
+++ turnuva.py
@@ -45,40 +45,39 @@ for i in range(4):
 with tabs[4]:
     st.header("🏆 Puan Durumu ve Veri Yedekleme")
     
-    col1, col2 = st.columns(2)
-    with col1:
-        uploaded_file = st.file_uploader("Kayıtlı dosyayı yükle", type="csv")
-        if uploaded_file is not None:
-            df_y = pd.read_csv(uploaded_file, index_col=0)
-            st.session_state.skorlar = {idx: list(row) for idx, row in df_y.iterrows()}
-            st.success("Veriler yüklendi! Lütfen sayfayı yenileyin.")
-    with col2:
-        df_yedek = pd.DataFrame.from_dict(st.session_state.skorlar, orient='index')
-        csv = df_yedek.to_csv()
-        st.download_button("📥 Verileri İndir", csv, "turnuva_yedek.csv", "text/csv")
-        
-    st.divider()
+    # [İndir/Yükle butonları buraya aynen kalacak...]
     
     secilen_grup = st.selectbox("Grup Seçiniz:", ["Grup 1", "Grup 2", "Grup 3", "Grup 4"])
     takimlar = st.session_state.takimlar[secilen_grup]
-    df = pd.DataFrame(0, index=takimlar, columns=["Seri Gal.", "Maç Alınan", "Maç Verilen", "Set Alınan", "Set Verilen", "Oyun Alınan", "Oyun Verilen"])
+    
+    # Sütunları tam istediğiniz detayda güncelledim
+    cols = ["Seri Gal.", "Alt Maç Alınan", "Alt Maç Verilen", "Set Alınan", "Set Verilen", "Set Averajı", "Oyun Alınan", "Oyun Verilen", "Oyun Averajı"]
+    df = pd.DataFrame(0, index=takimlar, columns=cols)
     
     for key, vals in st.session_state.skorlar.items():
         if secilen_grup in key:
             try:
                 n = [int(str(v)) for v in vals]
+                # 3 alt maçtan her birinin setleri (S1, S2, S3 setleri)
                 t1_sets = (1 if n[0]>n[1] else 0) + (1 if n[2]>n[3] else 0) + (1 if n[4]>n[5] else 0)
                 t2_sets = (1 if n[1]>n[0] else 0) + (1 if n[3]>n[2] else 0) + (1 if n[5]>n[4] else 0)
+                
                 t1, t2 = key.split('_')[2], key.split('_')[3]
+                
+                # Takım verilerini işle
                 if t1 in df.index:
+                    df.loc[t1, ["Alt Maç Alınan", "Alt Maç Verilen"]] += [1 if t1_sets > t2_sets else 0, 1 if t2_sets > t1_sets else 0]
                     df.loc[t1, ["Set Alınan", "Set Verilen", "Oyun Alınan", "Oyun Verilen"]] += [t1_sets, t2_sets, sum(n[0::2]), sum(n[1::2])]
-                    if t1_sets > t2_sets: df.loc[t1, "Maç Alınan"] += 1
-                    else: df.loc[t1, "Maç Verilen"] += 1
-                    if df.loc[t1, "Maç Alınan"] >= 2: df.loc[t1, "Seri Gal."] = 1
+                    if t1_sets > t2_sets and df.loc[t1, "Alt Maç Alınan"] % 3 == 2: df.loc[t1, "Seri Gal."] += 1
+                
                 if t2 in df.index:
+                    df.loc[t2, ["Alt Maç Alınan", "Alt Maç Verilen"]] += [1 if t2_sets > t1_sets else 0, 1 if t1_sets > t2_sets else 0]
                     df.loc[t2, ["Set Alınan", "Set Verilen", "Oyun Alınan", "Oyun Verilen"]] += [t2_sets, t1_sets, sum(n[1::2]), sum(n[0::2])]
-                    if t2_sets > t1_sets: df.loc[t2, "Maç Alınan"] += 1
-                    else: df.loc[t2, "Maç Verilen"] += 1
-                    if df.loc[t2, "Maç Alınan"] >= 2: df.loc[t2, "Seri Gal."] = 1
+                    if t2_sets > t1_sets and df.loc[t2, "Alt Maç Alınan"] % 3 == 2: df.loc[t2, "Seri Gal."] += 1
             except: continue
-    st.table(df.sort_values(by=["Seri Gal.", "Maç Alınan"], ascending=False))
+
+    # Averajları hesapla
+    df["Set Averajı"] = df["Set Alınan"] - df["Set Verilen"]
+    df["Oyun Averajı"] = df["Oyun Alınan"] - df["Oyun Verilen"]
+    
+    st.table(df.sort_values(by=["Seri Gal.", "Set Averajı", "Oyun Averajı"], ascending=False))
